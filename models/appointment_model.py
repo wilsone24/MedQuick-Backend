fc15ca4c8502212bee71d818c7a920c55b94f2ef
wilsone24 <wilsone@uninorte.edu.co>
2025-05-08 09:10:54 -0500

    Update Appointment model: change date and time fields to use Date and Time types

--- models/appointment_model.py
+++ models/appointment_model.py
@@ -1,4 +1,5 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Date, Time
+from sqlalchemy.orm import relationship
 from database import Base
 
 
@@ -8,7 +9,7 @@ class Appointment(Base):
     id_appointment = Column(Integer, primary_key=True, index=True, autoincrement=True)
     id_doctor = Column(Integer, nullable=False)
     id_patient = Column(Integer, nullable=False)
-    date = Column(String(10), nullable=False)
-    time = Column(String(5), nullable=False)
+    date = Column(Date, nullable=False)
+    time = Column(Time, nullable=False)
     description = Column(String(50), nullable=False)
     status = Column(String(10), nullable=False)
