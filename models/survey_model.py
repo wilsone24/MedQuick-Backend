a7830e230cce12d432fe014bcc2a3f9138511a77
wilsone24 <wilsone@uninorte.edu.co>
2025-05-21 20:58:08 -0500

    Update Survey model: change date and time fields to use Date and Time types

--- models/survey_model.py
+++ models/survey_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, String, Integer
+from sqlalchemy import Column, String, Integer, Date, Time
 from database import Base
 
 
@@ -7,7 +7,7 @@ class Survey(Base):
 
     id_survey = Column(Integer, primary_key=True, index=True, autoincrement=True)
     id_patient = Column(Integer, nullable=False)
-    date = Column(String(10), nullable=False)
-    time = Column(String(5), nullable=False)
+    date = Column(Date, nullable=False)
+    time = Column(Time, nullable=False)
     description = Column(String(100), nullable=False)
     sugestions = Column(String(50), nullable=False)
