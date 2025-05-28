96ebac01f665fc83b159814e9590308627e8992d
MatizS27 <samuel.matiz27@gmail.com>
2025-05-28 01:45:05 -0500

    Chat implementation

--- create_tables.py
+++ create_tables.py
@@ -1,3 +1,9 @@
 from database import Base, engine
+from models.user_model import User
+from models.patient_model import Patient
+from models.appointment_model import Appointment
+from models.doctor_model import Doctor
+from models.survey_model import Survey
+from models.specialty_model import Specialty
 
 Base.metadata.create_all(bind=engine)
